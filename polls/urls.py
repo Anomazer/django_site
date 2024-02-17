deda560fe07959f680257964f94fe03a1a9a7c2a
anomazer <kudrekoff@gmail.com>
2024-02-17 13:44:45 +0300

    new device commit

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,7 @@ from . import views, fun
 urlpatterns = [
     path('', views.index, name ='index'),
     path('fun/', fun.index, name = 'fun'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<in:question_id>/vote/', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,13 @@ def index(request):
     return HttpResponse(
         "<img src='https://images.techinsider.ru/upload/img_cache/bab/bab3324f6b0470e5156a1ebd5e48ce14_ce_1920x1280x0x0_cropped_666x444.jpg'>"
         "<style>img{width:1000px}")
+
+def detail(request, question_id):
+    return HttpResponse("You are seeing a question now %s." % question_id)
+
+def results(request, question_id):
+    responce = "You are seiing a question results now %s. "
+    return HttpResponse(responce % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting for question now %s." % question_id)
